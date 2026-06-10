6fc2ce5202b70818347834b913371dc49bcfbcd2
Josh252525 <josue100va@gmail.com>
2026-06-09 23:25:28 -0600

    Nearest Neighbor con enrutador y su test

---
+++ src/main/java/planner/EnrutadorNearestNeighbor.java
@@ -0,0 +1,80 @@
+package planner;
+
+import estructuras.LinkedList;
+
+public class EnrutadorNearestNeighbor {
+	
+	public LinkedList<Integer> GenerarRuta(int deposito, LinkedList<Integer> destinosCamion, double[][] matrizFloyd){
+		
+		if (matrizFloyd == null || matrizFloyd.length == 0) {
+            throw new IllegalArgumentException("Matriz Floyd inválida.");
+            
+        }
+		
+		LinkedList<Integer> ruta = new LinkedList<>();
+		
+		// Caso extremo: sin entregas
+        if (destinosCamion == null || destinosCamion.size() == 0) {
+            ruta.insert(deposito);
+            return ruta;
+        }
+        
+        int cantidadDestinos = destinosCamion.size();
+        boolean[] visitado = new boolean[cantidadDestinos];
+        
+        int ciudadActual = deposito;
+        
+     // Iniciamos en el depósito
+        ruta.insert(deposito);
+        
+        for(int EntregasRealizadas = 0; EntregasRealizadas < cantidadDestinos; EntregasRealizadas++) {
+        	
+        	double mejorDistancia = Double.POSITIVE_INFINITY;
+            int mejorIndice = -1;
+            
+         // Buscar el destino no visitado más cercano
+            for (int i = 0; i < cantidadDestinos; i++) {
+
+                if (visitado[i]) {
+                    continue;
+                }
+
+                int ciudadDestino = destinosCamion.getAt(i);
+
+                double distancia = matrizFloyd[ciudadActual][ciudadDestino];
+                
+                //Caso extremo: Si la distancia es infinita. 
+                if (distancia == Double.POSITIVE_INFINITY) {
+                    continue;
+                }
+                        
+
+                if (distancia < mejorDistancia) {
+                    mejorDistancia = distancia;
+                    mejorIndice = i;
+                }
+                
+                
+            }
+            
+         // Seguridad
+            if (mejorIndice == -1) {
+                break;
+            }
+            
+            visitado[mejorIndice] = true;
+            
+            ciudadActual = destinosCamion.getAt(mejorIndice);
+            
+            ruta.insert(ciudadActual);
+
+        }
+        
+     // Volver al depósito
+        ruta.insert(deposito);
+
+        return ruta;
+	}
+	
+
+}
